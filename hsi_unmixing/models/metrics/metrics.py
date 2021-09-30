3b58745e49b18712d947396f3313daaccb85f121
inzouzouwetrust <alexandre.zouaoui@free.fr>
2021-09-30 11:46:09 +0200

    Add metrics;

--- hsi_unmixing/models/metrics/metrics.py
+++ hsi_unmixing/models/metrics/metrics.py
@@ -25,6 +25,35 @@ def sad(inputs: Tensor, targets: Tensor, reduction="mean") -> Tensor:
         return angle.mean(0)
     return angle
 
+class SAD(nn.Module):
+  def __init__(self, num_bands: int=156):
+    super(SAD, self).__init__()
+    self.num_bands = num_bands
+
+  def forward(self, input, target):
+    """Spectral Angle Distance Objective
+    Implementation based on the mathematical formulation presented in 'https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=7061924'
+    
+    Params:
+        input -> Output of the autoencoder corresponding to subsampled input
+                tensor shape: (batch_size, num_bands)
+        target -> Subsampled input Hyperspectral image (batch_size, num_bands)
+        
+    Returns:
+        angle: SAD between input and target
+    """
+    try:
+      input_norm = torch.sqrt(torch.bmm(input.view(-1, 1, self.num_bands), input.view(-1, self.num_bands, 1)))
+      target_norm = torch.sqrt(torch.bmm(target.view(-1, 1, self.num_bands), target.view(-1, self.num_bands, 1)))
+      
+      summation = torch.bmm(input.view(-1, 1, self.num_bands), target.view(-1, self.num_bands, 1))
+      angle = torch.acos(summation/(input_norm * target_norm))
+      
+    
+    except ValueError:
+      return 0.0
+    
+    return angle
 
 def mse(inputs: Tensor, targets: Tensor) -> Tensor:
     assert inputs.shape == targets.shape
