d8d4a3cd04701f773b1002637f32c4bd4edf85fa
inzouzouwetrust <alexandre.zouaoui@free.fr>
2021-11-15 16:38:16 +0100

    Add losses;

--- hsi_unmixing/models/losses/losses.py
+++ hsi_unmixing/models/losses/losses.py
@@ -9,7 +9,7 @@ class ASC_penalty(nn.Module):
     def __init__(self, nu_init):
         super().__init__()
         self.nu = nn.Parameter(torch.ones(1))
-        nn.init.constant_(nu_init)
+        nn.init.constant_(self.nu, nu_init)
 
     def forward(self, alpha):
         return (self.nu / 2) * ((alpha.sum(1) - 1) ** 2).mean(0)
