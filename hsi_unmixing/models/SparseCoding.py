4718d1638b342cd145908a723f1823a101d6d50a
inzouzouwetrust <alexandre.zouaoui@free.fr>
2021-11-15 15:04:03 +0100

    First draft SC ASC PW;

--- hsi_unmixing/models/SparseCoding.py
+++ hsi_unmixing/models/SparseCoding.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 import pytorch_lightning as pl
 
 from .base import BaseModel
+from .losses import ASC_penalty
 
 
 logger = logging.getLogger(__name__)
@@ -129,6 +130,98 @@ class SparseCoding_pw(pl.LightningModule):
             ax[indx].imshow(abund)
             ax[indx].get_xaxis().set_visible(False)
             ax[indx].get_yaxis().set_visible(False)
+
+
+class SC_ASC_pw(nn.Module):
+
+    EPS = 1e-12
+
+    def __init__(
+        self,
+        n_bands,
+        n_endmembers,
+        unrollings,
+        # lambd_init=0.1,
+        nu_init=0.1,
+        gamma_init=0.1,
+        use_C=False,
+        use_W=False,
+    ):
+
+        super().__init__()
+        # Define D, eta, lambda
+        self.n_bands = n_bands
+        self.n_endmembers = n_endmembers
+        self.unrollings = unrollings
+        self.use_C = use_C
+        self.use_W = use_W
+
+        weight_init = self._init_dictionary()
+        self.D = nn.Linear(self.n_bands, self.n_endmembers, bias=False)
+        self.D.weight.data = weight_init
+
+        self.eta = nn.Parameter(torch.zeros(1))
+        # self.lambd = nn.Parameter(torch.zeros(1), requires_grad=False)
+        self.gamma = nn.Parameter(torch.zeros(1))
+        nn.init.constant_(self.eta, 1.0)
+        # nn.init.constant_(self.lambd, lambd_init)
+        nn.init.constant_(self.gamma, gamma_init)
+        self.ASC = ASC_penalty(nu_init)
+
+        if self.use_C:
+            self.C = nn.Linear(self.n_bands, self.n_endmembers, bias=False)
+            self.C.weight.data = weight_init
+        if self.use_W:
+            self.W = nn.Linear(self.n_bands, self.n_endmembers, bias=False)
+            self.W.weight.data = weight_init
+
+    # special routine to initialize D (needs to be positive)
+    def _init_dictionary(self):
+        D = torch.rand(size=(self.n_bands, self.n_endmembers))
+        dtd = D.t() @ D
+        e, _ = torch.symeig(dtd, eigenvectors=False)
+        D /= torch.sqrt(torch.max(e))
+        return D
+
+    def forward(self, x):
+        # Return sparse code and reconstruction
+
+        # Compute g and the first sparse code iterate
+        # (b, R)
+        if self.use_C:
+            g = F.linear(x, self.C.weight.t())
+        else:
+            g = self.eta * F.linear(x, self.D.weight.t())
+        alpha = F.relu(g + self.eta * self.gamma * torch.ones_like(g))
+        # (b, )
+        lambd = self.gamma * (alpha.sum(1) - 1)
+
+        # G trick
+        if self.unrollings > 1:
+            # retrieve the weight => (L, R)
+            D = self.D.weight
+            # build - eta * D.T() @ D => (R, R)
+            if self.use_C:
+                C = self.C.weight
+                G = torch.neg(C.t() @ D)
+            else:
+                G = self.eta * torch.neg(D.t() @ D)
+            G.diagonal().add_(1.0)  # add identity => (R, R)
+
+        # Unfoldings
+        for ii in range(self.unrollings - 1):
+            pre_alpha = g + F.linear(alpha, G)
+            penalty = (lambd + self.gamma * (alpha.sum(1) - 1)) * torch.ones_like(g)
+            alpha = F.relu(pre_alpha + penalty)
+            lambd = lambd + self.gamma * (alpha.sum(1) - 1)
+
+        # Compute reconstruction
+        # recon => (b, L)
+        if self.use_W:
+            recon = F.linear(alpha, self.W.weight)
+        else:
+            recon = F.linear(alpha, self.D.weight)
+        return recon, alpha
 
 
 def check_SC():
