6dbaa56bb6b8879b778f123c8eb9951d6adcdecd
inzouzouwetrust <alexandre.zouaoui@free.fr>
2021-11-17 17:14:07 +0100

    Modify ASC penalty to have fixed nu;

--- hsi_unmixing/models/losses/losses.py
+++ hsi_unmixing/models/losses/losses.py
@@ -1,15 +1,16 @@
 import torch
 import torch.nn as nn
 
-# def ASC_penalty(alpha, nu):
-#     return (nu / 2) * ((alpha.sum(1) - 1) ** 2).mean(0)
-
 
 class ASC_penalty(nn.Module):
+
+    EPS = 1e-8
+
     def __init__(self, nu_init):
         super().__init__()
-        self.nu = nn.Parameter(torch.ones(1))
+        self.nu = nn.Parameter(torch.ones(1), requires_grad=False)
         nn.init.constant_(self.nu, nu_init)
 
     def forward(self, alpha):
-        return (self.nu / 2) * ((alpha.sum(1) - 1) ** 2).mean(0)
+        nu = torch.clip(self.nu, self.EPS, 1.0)
+        return (nu / 2) * ((alpha.sum(1) - 1) ** 2).mean(0)
