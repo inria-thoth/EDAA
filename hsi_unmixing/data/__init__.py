9fe2533f2a9cb04f0cd0f24fcae92e8d37d1f44f
inzouzouwetrust <alexandre.zouaoui@free.fr>
2022-01-25 10:45:29 +0100

    Delete data files;

--- hsi_unmixing/data/__init__.py
+++
@@ -1,9 +0,0 @@
-from .Samson import Samson
-from .Urban import Urban
-from .Cuprite import Cuprite
-from .JasperRidge import JasperRidge
-from .Simulated import (
-    SimulatedPatches,
-    SimulatedPixels,
-    SimulatedDataCubes,
-)
