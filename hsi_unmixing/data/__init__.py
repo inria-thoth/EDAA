220e3b91452c21e5a8d0c00d3f8825267e385ff6
inzouzouwetrust <alexandre.zouaoui@free.fr>
2022-02-01 16:02:38 +0100

    Add noises/normalizers to data module;

---
+++ hsi_unmixing/data/__init__.py
@@ -0,0 +1,3 @@
+from .noises import AdditiveWhiteGaussianNoise as AWGN
+from .normalizers import (BandwiseMinMax, GlobalMinMax, PixelwiseL1Norm,
+                          PixelwiseL2Norm, RawInput)
