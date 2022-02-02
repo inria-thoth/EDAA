5d7d5e2d2eaec27d20089b05615f9b14bb866df2
inzouzouwetrust <alexandre.zouaoui@free.fr>
2022-02-02 16:00:40 +0100

    Add APEX6 to datasets;

--- hsi_unmixing/data/datasets/__init__.py
+++ hsi_unmixing/data/datasets/__init__.py
@@ -1,13 +1,7 @@
-from .APEX import APEX4Dataset
+from .APEX import APEX4Dataset, APEX6Dataset
 from .Cuprite import CupriteDataset
 from .JasperRidge import JasperRidgeDataset
 from .Samson import SamsonDataset
-from .Simulated import (
-    SimulatedDataCubesNoPurePixelsDataset,
-    SimulatedDataCubesNoPurePixelsHardDataset,
-)
-from .Urban import (
-    Urban4Dataset,
-    Urban5Dataset,
-    Urban6Dataset,
-)
+from .Simulated import (SimulatedDataCubesNoPurePixelsDataset,
+                        SimulatedDataCubesNoPurePixelsHardDataset)
+from .Urban import Urban4Dataset, Urban5Dataset, Urban6Dataset
