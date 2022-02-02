d44d419913389799f2ec1aa59bb3556e111bb6d4
inzouzouwetrust <alexandre.zouaoui@free.fr>
2022-02-02 15:54:58 +0100

    Fix APEX;

--- hsi_unmixing/data/datasets/APEX.py
+++ hsi_unmixing/data/datasets/APEX.py
@@ -9,17 +9,31 @@ logger.setLevel(logging.DEBUG)
 
 class APEX4Dataset(HSI):
     def __init__(self, **kwargs):
-        super().__init__("APEX4.mat", **kwargs)
+        # super().__init__("APEX4.mat", **kwargs)
+        super().__init__("APEX4fixed.mat", **kwargs)
 
 
 class APEX6Dataset(HSI):
     def __init__(self, **kwargs):
-        super().__init__("APEX6.mat", **kwargs)
+        # super().__init__("APEX6.mat", **kwargs)
+        super().__init__("APEX6fixed.mat", **kwargs)
 
 
 if __name__ == "__main__":
-    hsi = APEX4Dataset()
+    from hsi_unmixing.data.normalizers import GlobalMinMax as GMM
+    # from hsi_unmixing.data.normalizers import PixelwiseL1Norm as PL1
+    from hsi_unmixing.models.supervised import DecompSimplex as DS
+
+    setter = DS()
+    normalizer = GMM()
+    # normalizer = PL1()
+    hsi = APEX4Dataset(
+        normalizer=normalizer,
+        setter=setter,
+    )
     print(hsi)
-    # hsi.plot_endmembers(normalize=True)
-    hsi.plot_hsi(SNR=10)
+    hsi.plot_endmembers(normalize=False)
+    # hsi.plot_hsi(SNR=10)
     # hsi.plot_abundances(grid=(2, 3))
+
+    hsi.plot_abundances()
