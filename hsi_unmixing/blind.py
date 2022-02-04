f32ea0e9e2afd15ee4d9ca5b1d319dbef8e8f2c4
inzouzouwetrust <alexandre.zouaoui@free.fr>
2022-02-04 16:15:27 +0100

    Refactor main scripts;

--- hsi_unmixing/blind.py
+++ hsi_unmixing/blind.py
@@ -7,25 +7,28 @@ import time
 from hydra.utils import instantiate
 from omegaconf import OmegaConf
 
-from hsi_unmixing.data import AWGN
+# from hsi_unmixing.data import AWGN
 # from hsi_unmixing.models.initializers import VCA
 # from hsi_unmixing import data as data_utils
 # from hsi_unmixing.data import datasets
 from hsi_unmixing.models.metrics import aRMSE
-from hsi_unmixing.models.supervised import FCLS, DecompSimplex
+
+# from hsi_unmixing.models.supervised import FCLS, DecompSimplex
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
 def main(cfg):
-    print("Hello World!")
     logger.info(f"Current working directory: {os.getcwd()}")
     # print(OmegaConf.to_yaml(cfg))
     setter = instantiate(cfg.setter)
     normalizer = instantiate(cfg.normalizer)
     initializer = instantiate(cfg.initializer)
     model = instantiate(cfg.model)
+    noise = instantiate(cfg.noise)
+    criterion = instantiate(cfg.criterion)
+    metric = aRMSE()
 
     results = {}
 
@@ -35,7 +38,6 @@ def main(cfg):
             setter=setter,
             normalizer=normalizer,
         )
-        noise = AWGN()
         hsi.Y = noise.fit_transform(hsi.Y, SNR=cfg.SNR, seed=run)
 
         E0 = initializer.init_like(hsi, seed=run)
@@ -43,15 +45,22 @@ def main(cfg):
         aligner = instantiate(
             cfg.aligner,
             hsi=hsi,
-            criterion="MeanAbsoluteError",
+            criterion=criterion,
         )
+        E0, A0 = model.solve(hsi.Y, hsi.p, E0=E0)
+
         E1 = aligner.fit_transform(E0)
+        A1 = aligner.transform_abundances(A0)
 
-        A0 = model.solve(hsi.Y, E1)
-
-        metric = aRMSE()
-        res = metric(hsi.A, A0)
+        res = metric(hsi.A, A1)
         logging.info(f"aRMSE: {res:.2f}")
+        hsi.plot_endmembers(E0=E1)
+        hsi.plot_abundances(A0=A1)
+
+        if hasattr(model, "Xmap"):
+            X1 = aligner.transform_abundances(model.Xmap)
+            hsi.plot_contributions(X0=X1, method=model)
+
         results[run] = res
 
     logging.info(results)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import logging
 import os
 import pdb
+import shutil
 
 import hydra
+from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig
 
 logger = logging.getLogger(__name__)
@@ -12,9 +14,29 @@ logger.setLevel(logging.DEBUG)
 @hydra.main(config_path="hsi_unmixing/config", config_name="config")
 def main(cfg: DictConfig) -> None:
 
-    from hsi_unmixing.hello_world import main as _main
+    if os.path.exists("config.yaml"):
+        logger.info("Loading pre-existing config file")
+        hydra_cfg = HydraConfig.get()
+        overrides = hydra_cfg.overrides.task
+        cfg = hydra.compose("config.yaml", overrides=overrides)
+    else:
+        # copy initial config to a separate file to avoid overwriting it
+        # when hydra resumes training and initializes again
+        shutil.copy2("./hydra/config.yaml", "config.yaml")
 
-    _main(cfg)
+    mode = cfg.mode
+
+    if mode == "blind":
+        from hsi_unmixing.blind import main as _main
+    elif mode == "supervised":
+        from hsi_unmixing.supervised import main as _main
+    else:
+        raise ValueError(f"Mode {mode} is invalid")
+
+    try:
+        _main(cfg)
+    except Exception as e:
+        logger.critical(e, exc_info=True)
 
 
 if __name__ == "__main__":
