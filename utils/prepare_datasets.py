5ebce7c39d8b7977a194e5fb5d97927eb960f016
azouaoui <alexandre.zouaoui@free.fr>
2022-09-20 10:40:19 +0200

    fix(download): data available from pascal server

--- utils/prepare_datasets.py
+++ utils/prepare_datasets.py
@@ -1,11 +1,17 @@
 import os
+import subprocess
 import zipfile
 
 DATA_PATH = "./data.zip"
 TARGET_DIR = "./data/"
+URL = "http://pascal.inrialpes.fr/data2/azouaoui/data.zip"
 
 
 def main():
+
+    if not os.path.exists(DATA_PATH):
+        cmd_dl = f"wget --no-check-certificate {URL!r} -O {DATA_PATH!r}"
+        subprocess.check_call(cmd_dl, shell=True)
     assert os.path.exists(DATA_PATH)
     if os.path.exists(TARGET_DIR):
         print(f"{TARGET_DIR} already exists. Aborting...")
